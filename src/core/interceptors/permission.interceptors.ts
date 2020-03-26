117cac00c65701a8b6d02263f252632870003908
Innei <tukon479@gmail.com>
2020-03-26 20:02:29 +0800

    feat(permission): guest and master.

---
+++ src/core/interceptors/permission.interceptors.ts
@@ -0,0 +1,26 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+  BadRequestException,
+} from '@nestjs/common'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+import { AnyType } from 'src/shared/base/interfaces'
+
+@Injectable()
+export class PermissionInterceptor<T> implements NestInterceptor<T, AnyType> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<AnyType> {
+    const http = context.switchToHttp()
+    const req = http.getRequest()
+    return next.handle().pipe(
+      map(data => {
+        if (data.hide === true && !req.isMaster) {
+          throw new BadRequestException('你.是我的主人吗 ಠ_ಠ')
+        }
+        return data
+      }),
+    )
+  }
+}
