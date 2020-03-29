b906c02a281213da0d1a259b3ba71d39190b1860
Innei <tukon479@gmail.com>
2020-03-29 20:20:17 +0800

    feat(comment): reply comment

--- src/shared/comments/comments.service.ts
+++ src/shared/comments/comments.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { BaseService } from '../base/base.service'
+import Comment from '@libs/db/models/comment.model'
+import { InjectModel } from 'nestjs-typegoose'
+import { ReturnModelType } from '@typegoose/typegoose'
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService extends BaseService<Comment> {
+  constructor(
+    @InjectModel(Comment)
+    private readonly commentModel: ReturnModelType<typeof Comment>,
+  ) {
+    super(commentModel)
+  }
+}
