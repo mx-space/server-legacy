9b4d4a6b6ca6344ce70c87698656bcbf91daecc0
Innei <tukon479@gmail.com>
2020-04-24 20:42:16 +0800

    feat: file upload via type

--- libs/db/src/models/file.model.ts
+++ libs/db/src/models/file.model.ts
@@ -1,4 +1,4 @@
-import { prop } from '@typegoose/typegoose'
+import { prop, index } from '@typegoose/typegoose'
 import { Schema } from 'mongoose'
 
 export interface Dimensions {
@@ -6,6 +6,34 @@ export interface Dimensions {
   width: number
   type: string
 }
+
+export enum FileType {
+  // image
+  IMAGE,
+  AVATAR,
+  BACKGROUND,
+  PHOTO,
+
+  // not image
+  // MUSIC,
+  // VIDEO,
+  // FILE,
+}
+export const getFileType = (type: FileType) => {
+  const ft = Object.keys(FileType)
+  return ft.splice(ft.length / 2)[type].toLowerCase()
+}
+export const getEnumFromType = (type: keyof typeof FileType) => {
+  return {
+    IMAGE: 0,
+    AVATAR: 1,
+    BACKGROUND: 2,
+    PHOTO: 3,
+  }[type]
+}
+
+@index({ filename: 1 })
+@index({ name: 1 })
 export class File {
   @prop({ required: true })
   filename: string
@@ -21,5 +49,7 @@ export class File {
 
   @prop()
   dimensions?: Dimensions
-}
 
+  @prop({ default: FileType.IMAGE })
+  type: number
+}
--- src/shared/uploads/uploads.controller.ts
+++ src/shared/uploads/uploads.controller.ts
@@ -1,17 +1,22 @@
+import { FileType, getEnumFromType } from '@libs/db/models/file.model'
 import {
   Controller,
+  Delete,
+  Get,
+  Param,
   Post,
+  Query,
   Req,
   Res,
-  BadRequestException,
-  Param,
-  Get,
 } from '@nestjs/common'
-import { ApiConsumes, ApiProperty, ApiBody, ApiTags } from '@nestjs/swagger'
-import { UploadsService } from 'src/shared/uploads/uploads.service'
-import { FastifyRequest, FastifyReply } from 'fastify'
+import { ApiBody, ApiConsumes, ApiProperty, ApiTags } from '@nestjs/swagger'
+import { FastifyReply, FastifyRequest } from 'fastify'
 import { IncomingMessage, ServerResponse } from 'http'
 import { Auth } from 'src/core/decorators/auth.decorator'
+import { CannotFindException } from 'src/core/exceptions/cant-find.exception'
+import { UploadsService } from 'src/shared/uploads/uploads.service'
+import { IdDto } from '../base/dto/id.dto'
+import { FileTypeQueryDto } from './dto/filetype.dto'
 class FileUploadDto {
   @ApiProperty({ type: 'string', format: 'binary' })
   file: any
@@ -29,36 +34,43 @@ export class UploadsController {
     type: FileUploadDto,
   })
   // @Auth()
-  async uploadImage(@Req() req: FastifyRequest<IncomingMessage>) {
-    if (!req.isMultipart()) {
-      throw new BadRequestException('仅供上传文件!')
-    }
-    if (!req.body.file) {
-      throw new BadRequestException('字段必须为 file')
-    }
-    const fileInfo = req.body.file[0]
-    if (!fileInfo) {
-      throw new BadRequestException('文件丢失了')
-    }
-    if (fileInfo.limit) {
-      throw new BadRequestException('文件不符合, 大小不得超过 6M')
-    }
-    const data = await this.service.saveImage(fileInfo)
+  async uploadImage(
+    @Req() req: FastifyRequest<IncomingMessage>,
+    @Query() query: FileTypeQueryDto,
+  ) {
+    const { type = FileType.IMAGE } = query
+    const fileInfo = this.service.ValidImage(req)
+    const data = await this.service.saveImage(fileInfo, type)
     return { ...data }
   }
 
-  @Get('image/:hashname')
+  @Get(':type/:hashname')
   async getImage(
     @Param('hashname') name: string,
+    @Param('type') _type: string,
     @Res() res: FastifyReply<ServerResponse>,
   ) {
-    const { buffer, mime } = await this.service.checkFileExist(
-      name,
-      this.service.imagePath,
-    )
+    const type = getEnumFromType(_type.toUpperCase() as keyof typeof FileType)
+    if (!(type in FileType)) {
+      throw new CannotFindException()
+    }
+
+    const { buffer, mime } = await this.service.checkFileExist(name, type)
     const stream = this.service.getReadableStream(buffer)
     res.type(mime).send(stream)
   }
 
-  // TODO: 增加删除和获取信息的接口  <13-04-20 Innei> //
+  @Get('image/info/:hashname')
+  async getImageInfo(
+    @Param('hashname') name: string,
+    @Query() query: FileTypeQueryDto,
+  ) {
+    const { type = FileType.IMAGE } = query
+    return await this.service.getImageInfo(name, type)
+  }
+  @Delete(':id')
+  async deleteFile(@Param() param: IdDto) {
+    const { id } = param
+    return this.service.deleteFile(id)
+  }
 }
