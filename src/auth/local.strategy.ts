4bcaafbf031330fc738ab445db7df3eb598e3379
Innei <tukon479@gmail.com>
2020-04-26 13:28:17 +0800

    feat: comment vaild and wait for error login

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -6,6 +6,10 @@ import { compareSync } from 'bcrypt'
 import { InjectModel } from 'nestjs-typegoose'
 import { IStrategyOptions, Strategy } from 'passport-local'
 
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
 export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
   constructor(
     @InjectModel(User) private userModel: ReturnModelType<typeof User>,
@@ -19,9 +23,11 @@ export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
   async validate(username: string, password: string) {
     const user = await this.userModel.findOne({ username }).select('+password')
     if (!user) {
+      await sleep(3000)
       throw new ForbiddenException('用户名不正确')
     }
     if (!compareSync(password, user.password)) {
+      await sleep(3000)
       throw new ForbiddenException('密码不正确')
     }
     // console.log(user)
--- src/shared/comments/dto/comment.dto.ts
+++ src/shared/comments/dto/comment.dto.ts
@@ -6,6 +6,7 @@ import {
   IsString,
   IsUrl,
   IsEnum,
+  MaxLength,
 } from 'class-validator'
 import { CommentRefTypes } from '@libs/db/models/comment.model'
 
@@ -13,22 +14,26 @@ export class CommentDto {
   @IsString()
   @IsNotEmpty()
   @ApiProperty()
+  @MaxLength(20, { message: '昵称不得大于 20 个字符' })
   author: string
 
   @IsString()
   @IsNotEmpty()
   @ApiProperty()
+  @MaxLength(200, { message: '评论内容不得大于 200 个字符' })
   text: string
 
   @IsString()
-  @IsEmail()
+  @IsEmail(undefined, { message: '请更正为正确的邮箱' })
   @ApiProperty({ example: 'test@mail.com' })
+  @MaxLength(50, { message: '邮箱地址不得大于 50 个字符' })
   mail: string
 
   @IsString()
   @IsUrl({ require_protocol: true }, { message: '请更正为正确的网址' })
   @IsOptional()
   @ApiProperty({ example: 'http://example.com' })
+  @MaxLength(50, { message: '地址不得大于 50 个字符' })
   url?: string
 }
 
