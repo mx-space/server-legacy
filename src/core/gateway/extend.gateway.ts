949660ef04ffbdb733b90f513af8c787aa7d0481
Innei <tukon479@gmail.com>
2020-08-03 10:53:18 +0800

    fix: remove ws auth middleware

--- src/core/gateway/extend.gateway.ts
+++ src/core/gateway/extend.gateway.ts
@@ -1,11 +1,18 @@
+/*
+ * @Author: Innei
+ * @Date: 2020-05-06 11:57:22
+ * @LastEditTime: 2020-08-03 10:53:02
+ * @LastEditors: Innei
+ * @FilePath: /mx-server/src/core/gateway/extend.gateway.ts
+ * @Coding with Love
+ */
 import { IoAdapter } from '@nestjs/platform-socket.io'
-import { gatewayAuthMiddleware } from '../middlewares/gateway-auth.middleware'
 import type { Server } from 'socket.io'
 
 export class ExtendsIoAdapter extends IoAdapter {
   createIOServer(port: number, options?: any): any {
     const server = super.createIOServer(port, options) as Server
-    server.use(gatewayAuthMiddleware)
+
     return server
   }
 }
