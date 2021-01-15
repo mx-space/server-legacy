3f87b56efaedc6ed78730ecd501ba0a4cd01db8f
Innei <tukon479@gmail.com>
2021-01-15 14:52:52 +0800

    ci: update release script

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@ set -e
 ###
 # @Author: Innei
 # @Date: 2020-09-17 14:04:22
-# @LastEditTime: 2021-01-15 14:25:50
+# @LastEditTime: 2021-01-15 14:51:43
 # @LastEditors: Innei
 # @FilePath: /server/release.sh
 # @Mark: Coding with Love
@@ -10,6 +10,6 @@ set -e
 yarn
 yarn build:server
 yarn add @zeit/ncc
-yarn run ncc build dist/server/main.js -o release
+yarn run ncc build dist/apps/server/main.js -o release
 cp .env.example release/.env
 zip -r release.zip release/* release/.env
