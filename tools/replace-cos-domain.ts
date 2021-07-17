8f5ecbf2341594bede55c3458bf0871c60c87fcc
Innei <tukon479@gmail.com>
2021-07-17 22:33:58 +0800

    chore: replace cdn domain

---
+++ tools/replace-cos-domain.ts
@@ -0,0 +1,47 @@
+import Note from '../libs/db/src/models/note.model'
+import Post from '../libs/db/src/models/post.model'
+import { getModelForClass } from '@typegoose/typegoose'
+import { config } from 'dotenv'
+import { connect, disconnect } from 'mongoose'
+const env = config().parsed
+
+async function main() {
+  connect((env.DB_URL || 'mongodb://localhost') + '/mx-space', {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    autoIndex: true,
+  })
+
+  {
+    const post = getModelForClass(Post)
+    const note = getModelForClass(Note)
+    const allPosts = await post.find()
+    const allNotes = await note.find()
+
+    for await (const post of [...allPosts, ...allNotes]) {
+      const text = post.text
+
+      post.text = text.replace(
+        /\(https:\/\/tu-1252943311\.cos\.accelerate\.myqcloud\.com\//g,
+        '(https://cdn.innei.ren/',
+      )
+
+      post.images = post.images.map((image) => {
+        image.src = image.src.replace(
+          /\https:\/\/tu-1252943311\.cos\.accelerate\.myqcloud\.com\//g,
+          'https://cdn.innei.ren/',
+        )
+        return image
+      })
+
+      //@ts-ignore
+      await post.save()
+    }
+  }
+
+  disconnect()
+}
+
+main()
